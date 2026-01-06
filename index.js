aa8f1e92d0ced8138266a81a69849619cb669860
DracFiendMG <sreeramrdy@gmail.com>
2026-01-06 20:38:15 +0530

    Enhance movie rendering and error handling: update movie details fetching, improve layout, and add error display section

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
 let movies = []
+let movieDetails = []
+
+const movieSection = document.querySelector(".movie-section")
+const errorSection = document.querySelector(".error-section")
 
 fetch("http://www.omdbapi.com/?apikey=f000f77b&s=Blade Runner")
     .then(res => res.json())
     .then(data => {
         console.log(data.Search)
+        if (data.Search.length === 0) {
+            throw new Error("No movies found")
+        }
         movies = data.Search
-        renderMovies()
+        movieSection.style.display = 'flex'
+        errorSection.style.display = 'none'
+        fetchMovieDetails()
+    })
+    .catch(error => {
+        movieSection.style.display = 'none'
+        errorSection.style.display = 'flex'
+        renderError()
     })
 
-function fetchMovieDetails(imdbId) {
-    return fetch(`http://www.omdbapi.com/?i=${imdbId}&apikey=f000f77b`)
-        .then(res => res.json())
-        .then(data => data)
+function fetchMovieDetails() {
+    const moviePromises = movies.map(async movie => {
+        return await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=f000f77b`)
+            .then(res => res.json())
+    })
+
+    Promise.all(moviePromises)
+        .then(details => {
+            movieDetails = details
+            console.log(movieDetails)
+            renderMovies()
+        })
 }
 
 function renderMovies() {
-    let moviesHtml = movies.map(movie => {
+    let moviesHtml = movieDetails.map(movie => {
         return `
             <div class="movie-card">
                 <img src="${movie.Poster}">
                 <div class="content">
                     <div class="head">
                         <h3>${movie.Title}</h3>
-                        <p>8.1</p>
+                        <p>${movie.imdbRating !== 'N/A' ? '<i class="fa-solid fa-star"></i>' : ''}${movie.imdbRating}</p>
                     </div>
                     <div class="subhead">
-                        <p>116min</p>
-                        <p>Drama, Mystery, Sci-fi</p>
-                        <p>Watchlist</p>
+                        <p>${movie.Runtime}</p>
+                        <p>${movie.Genre}</p>
+                        <p class="watchlist">
+                            <i class="fa-solid fa-circle-plus"></i>
+                            <!-- <i class="fa-solid fa-circle-check"></i> -->
+                            Watchlist
+                        </p>
                     </div>
-                    <p class="desc">Description</p>
+                    <p class="desc">${movie.Plot}</p>
                 </div>
             </div>
         `
-    }).join('')
-    document.querySelector(".movie-section").innerHTML = moviesHtml
+    }).join("")
+
+    movieSection.innerHTML = moviesHtml
+}
+
+function renderError() {
+    document.querySelector(".error-section").innerHTML = `
+        <p>Unable to find what you’re looking for. Please try another search.</p>
+    `
 }
